01a28126f212ee7c3bf390d07f1ad028711f4889
franciowel <spina_francesco@blu.it>
2022-07-18 10:28:24 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComponents :headerLinks="headerLinks"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponents from './components/HeaderComponents.vue'
 
 export default {
+  data () {
+    return {
+      headerLinks: [
+        {
+          text: 'Home',
+          link: '#'
+        },
+        {
+          text: 'About',
+          link: '#'
+        },
+        {
+          text: 'Services',
+          link: '#'
+        },
+        {
+          text: 'Blog',
+          link: '#'
+        },
+        {
+          text: 'Contact',
+          link: '#'
+        },
+        {
+          text: 'Portfolio',
+          link: '#'
+        },
+      ]
+    }
+  },
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComponents,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/HeaderComponents.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <div class="contain">
+            <div class="navbar">
+                <!-- logo -->
+                <div class="logo">
+                    <img src="../assets/img/white-logo-2.png" alt="Logo Phlox Corporate">
+                </div>
+                <!-- links -->
+                <div>
+                    <a :href="item.link" v-for="item, index in headerLinks" :key="index">{{item.text}}</a>
+                </div>
+                <!-- sign up -->
+                <div>
+                    <a href="#">Sign Up</a>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponents',
+    props: {
+        headerLinks: Array,
+    }
+}
+</script>
